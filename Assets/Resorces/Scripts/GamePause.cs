2ba5f0058cbcd348310f567f4bc5fdcff20d244c
go0n87 <go0n@mail.ru>
2024-01-16 02:35:39 +0300

    2:35 16.01.2024

---
+++ Assets/Resorces/Scripts/GamePause.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePause : MonoBehaviour
+{
+    public Sprite PauseButton;
+    public Sprite PlayButton;
+    public GameObject MenuPanel;
+    private Image _currentImage;
+    private bool _isPause = false;
+
+    private void Start()
+    {
+        _currentImage = GetComponent<Image>();
+    }
+
+    public void OnClickPause()
+    {        
+        if (_isPause)
+        {
+            Time.timeScale = 1;
+            _currentImage.sprite = PlayButton;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            _currentImage.sprite = PauseButton;            
+        }
+
+        MenuPanel.SetActive(!_isPause);
+        _isPause = !_isPause;
+    }
+}
